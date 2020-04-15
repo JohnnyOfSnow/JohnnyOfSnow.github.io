54294ac0187a0b8fdeeaf98c1ef4b0cf2d379666
johnnyOfSnow <good258johnny@yahoo.com.tw>
2020-04-15 15:43:47 +0900

    Add language choose

---
+++ tsuno/languageData.js
@@ -0,0 +1,86 @@
+﻿/* All Language Data */
+/* If there are some wrong, welcome to use pull request to tell me. Thanks.*/
+
+/* Traditional Chinese - Taiwan */
+L_taiwan = new Array(); 
+L_taiwan[0] = "角卷棉芽猜拳"; 
+L_taiwan[1] = "音量大小:"; 
+L_taiwan[2] = "請按下開始按鈕"; 
+L_taiwan[3] = "開始"; 
+L_taiwan[4] = "再來一局"; 
+L_taiwan[5] = "猜拳倒數時間："; 
+L_taiwan[6] = "在時間結束之前，按下要出的拳。"; 
+L_taiwan[7] = "影片載入中，請稍等";
+L_taiwan[8] = "剪刀";
+L_taiwan[9] = "石頭";
+L_taiwan[10] = "布";
+L_taiwan[11] = "你輸";
+L_taiwan[12] = "你贏";
+L_taiwan[13] = "平手";
+L_taiwan[14] = "你出的拳是:";
+L_taiwan[15] = "角卷棉芽出的拳是:";
+L_taiwan[16] = "因為你沒有出拳";
+L_taiwan[17] = "語言";
+
+/* Simplified Chinese */
+L_china = new Array(); 
+L_china[0] = "角卷棉芽猜拳"; 
+L_china[1] = "音量大小:"; 
+L_china[2] = "请按下开始按钮"; 
+L_china[3] = "开始"; 
+L_china[4] = "再来一局"; 
+L_china[5] = "猜拳倒数时间："; 
+L_china[6] = "在时间结束之前，按下要出的拳。"; 
+L_china[7] = "影片载入中，请稍等";
+L_china[8] = "剪刀";
+L_china[9] = "石头";
+L_china[10] = "布";
+L_china[11] = "你输";
+L_china[12] = "你贏";
+L_china[13] = "平手";
+L_china[14] = "你出的拳是:";
+L_china[15] = "角卷棉芽出的拳是:";
+L_china[16] = "因为你没有出拳";
+L_china[17] = "语言";
+
+/* English */
+L_english = new Array(); 
+L_english[0] = "Tsunomaki rock, paper, scissors"; 
+L_english[1] = "Volume:"; 
+L_english[2] = "Please press the start button"; 
+L_english[3] = "Start"; 
+L_english[4] = "Again"; 
+L_english[5] = "Game time:"; 
+L_english[6] = "Press your choice before run out of game time"; 
+L_english[7] = "Video is loading. Please wait.";
+L_english[8] = "Scissors";
+L_english[9] = "Rock";
+L_english[10] = "Paper";
+L_english[11] = "You LOSE";
+L_english[12] = "You WIN";
+L_english[13] = "DRAW";
+L_english[14] = "Your choice: ";
+L_english[15] = "Tsunomaki choice: ";
+L_english[16] = "You don't choice!";
+L_english[17] = "Language";
+
+/* Japanese */
+L_japanese = new Array(); 
+L_japanese[0] = "つのまきじゃんけん"; 
+L_japanese[1] = "音量レベル:"; 
+L_japanese[2] = "スタートボタンを押してください。"; 
+L_japanese[3] = "スタート"; 
+L_japanese[4] = "もう一回"; 
+L_japanese[5] = "カウントダウン時間:"; 
+L_japanese[6] = "時間が終わる前に、思いついたパンチを押します。"; 
+L_japanese[7] = "ビデオを読み込んでいます。お待ちください。";
+L_japanese[8] = "チョキ";
+L_japanese[9] = "グー";
+L_japanese[10] = "パー";
+L_japanese[11] = "負け";
+L_japanese[12] = "勝つ";
+L_japanese[13] = "あいこ";
+L_japanese[14] = "あなた:";
+L_japanese[15] = "つのまき:";
+L_japanese[16] = "あなたはじゃんけんしない。";
+L_japanese[17] = "言語";
