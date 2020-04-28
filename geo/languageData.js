f6f8d4a5c907fd0f0adbc60ffa19c61ece2a09fe
johnnyOfSnow <good258johnny@yahoo.com.tw>
2020-04-28 13:13:07 +0900

    Add question text

---
+++ geo/languageData.js
@@ -0,0 +1,57 @@
+﻿/* All Language Data */
+/* If there are some wrong, welcome to use pull request to tell me. Thanks.*/
+
+/* latitude_answer */
+latitude_answer = new Array(); 
+latitude_answer[0] = 0; 
+latitude_answer[1] = 24.9803434; 
+latitude_answer[2] = 24.9802741; 
+latitude_answer[3] = 0; 
+latitude_answer[4] = 24.9799443; 
+latitude_answer[5] = 24.978032; 
+latitude_answer[6] = 24.9796983; 
+latitude_answer[7] = 24.981872;
+latitude_answer[8] = 24.981856;
+latitude_answer[9] = 24.9817377;
+latitude_answer[10] = 24.9824334;
+latitude_answer[11] = 24.9822743;
+latitude_answer[12] = 24.9818728;
+latitude_answer[13] = 24.9818415;
+latitude_answer[14] = 24.9824694;
+
+
+/* longitude_answer */
+longitude_answer = new Array(); 
+longitude_answer[0] = 0; 
+longitude_answer[1] = 121.4256937; 
+longitude_answer[2] = 121.4242251; 
+longitude_answer[3] = 0; 
+longitude_answer[4] = 121.4279398; 
+longitude_answer[5] = 121.4304399; 
+longitude_answer[6] = 121.426154; 
+longitude_answer[7] = 121.4268403;
+longitude_answer[8] = 121.4268184;
+longitude_answer[9] = 121.4280036;
+longitude_answer[10] = 121.4273797;
+longitude_answer[11] = 121.4275345;
+longitude_answer[12] = 121.4268397;
+longitude_answer[13] = 121.4267098;
+longitude_answer[14] = 121.4268759;
+
+/* question */
+question_text = new Array(); 
+question_text[0] = ""; 
+question_text[1] = "沒有行人號誌燈的危險出入口"; 
+question_text[2] = "綠色的水池有好多魚"; 
+question_text[3] = ""; 
+question_text[4] = "一覽無遺孩童玩沙景象"; 
+question_text[5] = "要是這裡有陽光，肯定很漂亮"; 
+question_text[6] = "左邊一排椰子樹"; 
+question_text[7] = "也許你會看到一棵木瓜樹";
+question_text[8] = "停車場的進入點";
+question_text[9] = "請面向一排涼亭的巨石群";
+question_text[10] = "體育場就是要站在45度角位置才漂亮";
+question_text[11] = "銅像打網球";
+question_text[12] = "寶礦力水得一定是這座公園的象徵";
+question_text[13] = "追求極限的您一定要造訪這裡";
+question_text[14] = "極限的景物";
