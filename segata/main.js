b50250168b89976de247ec736af74f5415a3202e
johnnyOfSnow <good258johnny@yahoo.com.tw>
2020-05-15 15:13:43 +0900

    Complete the trangle up and down, and get their location

--- segata/main.js
+++ segata/main.js
@@ -7,9 +7,25 @@ var wordLocationX = 662;
 var wordLocationY = 31;
 var canvas;
 var ctx;
-var move = 0;
+var move = 0; // best = 165
 var state = 0;
 var timer;
+var timer_delta = 0.1;
+var trangle_delta = 1;
+const READY = 0;
+const UP = 1;
+const DOWN = 2;
+const STOP = 3;
+const NEXT = 4;
+const BEST = 165;
+const MIN = 326;
+const MAX = 331;
+
+
+
+var firstTrangle = 0;
+var secondTrangle = 0;
+
 
 window.onload = function() {
     init();
@@ -25,15 +41,45 @@ function init() {
 
 
 function draw() {
+
   ctx = canvas.getContext('2d');
   ctx.clearRect(0, 0, canvas.width, canvas.height);
 
   drawRectBar();
 
-  drawTrangle();
 
-  drawText('Caution', wordLocationX, wordLocationY + rect_width, 15, 'Comic Sans MS');
-  drawText('Caution', wordLocationX, wordLocationY + rect_width * 13 , 15,'Comic Sans MS');
+  switch(state){
+  	case READY:
+  		drawTrangle(getMove(), '#642100');
+  	break;
+  	case UP:
+  		drawTrangle(getMove(), '#642100');
+  	break;
+  	case DOWN:
+  		drawTrangle(firstTrangle, '#0000C6');
+  		drawTrangle(getMove(), '#642100');
+  	break;
+  	case STOP:
+  		drawTrangle(firstTrangle, '#0000C6');
+  		drawTrangle(secondTrangle, '#642100');
+	  	drawText(Math.abs(firstTrangle).toString(), 20, 20 ,20, 'Comic Sans MS', "black");
+	  	drawText(secondTrangle.toString(), 20, 80 ,20, 'Comic Sans MS', "black");
+	    drawText(judge(Math.abs(firstTrangle), secondTrangle), 20, 120 ,20, 'Comic Sans MS', "black");
+	  	
+  	break;
+  	case NEXT:
+  		drawTrangle(getMove(), '#642100');
+  	break;
+  	default:
+
+  	break;
+  }
+
+  
+
+
+  drawText('Caution', wordLocationX, wordLocationY + rect_width, 15, 'Comic Sans MS', "white");
+  drawText('Caution', wordLocationX, wordLocationY + rect_width * 13 , 15,'Comic Sans MS', "white");
 }
 
 
@@ -53,27 +99,29 @@ function drawRectBar(){
 }
 
 
-function drawText(text,centerX,centerY,fontsize,fontface){
+function drawText(text,centerX,centerY,fontsize,fontface,fontcolor){
   ctx.save();
   ctx.font=fontsize+'px '+fontface;
-  ctx.fillStyle = "white";
+  ctx.fillStyle = fontcolor;
   ctx.fillText(text,centerX,centerY);
   ctx.restore();
 }
 
 // Trangle
-function drawTrangle(){
+function drawTrangle(move, color){
   
-
   const trangleX = rectLocationX - 5;
   const trangleY = rectLocationY + (rect_width * 6) + (rect_width / 2);
 
+  ctx.fillStyle = color;
   ctx.beginPath();
-  ctx.moveTo(trangleX, trangleY + getMove());
-  ctx.lineTo(trangleX - 15, trangleY - 10 + getMove());
-  ctx.lineTo(trangleX - 15, trangleY + 10 + getMove());
+  ctx.moveTo(trangleX, trangleY + move);
+  ctx.lineTo(trangleX - 15, trangleY - 10 + move);
+  ctx.lineTo(trangleX - 15, trangleY + 10 + move);
   ctx.fill();
 }
+
+
 
 function setMove(newMove){
 	move = newMove;
@@ -84,21 +132,65 @@ function getMove(){
 }
 
 function mouseDownHandler(event){
-    if(state == 0){
-    	state = 1;
-    	timer = setInterval(controlTrangle, 0.1);
-	}else{
-		// Stop the trangle (every round in the second click)
-		clearInterval(timer);
+
+	switch(state){
+  	case READY:
+  		state = UP;
+    	timer = setInterval(controlTrangleUp, timer_delta);
+  	break;
+  	case UP:
+  		state = DOWN;
+    	firstTrangle = move;
+    	clearInterval(timer);
+    	timer = setInterval(controlTrangleDown, timer_delta);
+  	break;
+  	case DOWN:
+  		clearInterval(timer);
+		secondTrangle = move;
+		state = STOP;
 		draw();
+  	break;
+  	case STOP:
+		setMove(0);
+		draw();
+		state = READY;
+  	break;
+  	case NEXT:
+  	
+  	break;
+  	default:
+
+  	break;
+  }
+}
+
+function controlTrangleUp(){
+	if(move > -195){
+		setMove(move - trangle_delta); 
+		draw();
+	}else{
+		clearInterval(timer);
 	}
 }
 
-function controlTrangle(){
-	if(move > -200){
-		setMove(move - 1); 
+function controlTrangleDown(){
+	if(move < 195){
+		setMove(move + trangle_delta); 
 		draw();
 	}else{
 		clearInterval(timer);
 	}
+}
+
+function judge(first, second){
+	if(first > BEST || second > BEST){
+		return "失敗";
+	}else{
+		if( (first+second) > MIN  &&  (first+second) < MAX){
+			return "完美";
+		}else{
+			return "失敗";
+		}
+	}
+	
 }
